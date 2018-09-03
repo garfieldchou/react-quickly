69c452b979aa086467e12c890ea8f2e56ccd2061
garfield <cmgarfield@gmail.com>
2018-09-03 14:53:08 +0800

    Enhance Tooltip by making it take a property that positions the help text above the text instead of in the default position below the text

--- ch10/tooltip/script.jsx
+++ ch10/tooltip/script.jsx
@@ -16,9 +16,10 @@ class Tooltip extends React.Component {
     const style = {
       zIndex: (this.state.opacity) ? 1000 : -1000,
       opacity: +this.state.opacity,
-      top: (this.state.top || 0) + 20,
+      top: (this.state.top || 0) - this.props.moveTop,
       left: (this.state.left || 0) - 30
     }
+    console.log(style.top, style.left);
     return (
       <div style={{display: 'inline'}}>
         <span style={{color: 'blue'}}
@@ -28,7 +29,7 @@ class Tooltip extends React.Component {
           >
           {this.props.children}
         </span>
-        <div className="tooltip bottom" style={style} role="tooltip">
+        <div className="tooltip top" style={style} role="tooltip">
           <div className="tooltip-arrow"></div>
           <div className="tooltip-inner">
             {this.props.text}
@@ -39,11 +40,11 @@ class Tooltip extends React.Component {
   }
 }
 
-Tooltip.defaultProps = {overOrClick: 'over'}
+Tooltip.defaultProps = {overOrClick: 'over', moveTop: 30}
 
 ReactDOM.render(<div>
-  <Tooltip text="Master Express.js-The Node.js Framework For Your Web Development">Pro Express.js</Tooltip> was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress
-    after <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps" overOrClick="click">Practical Node.js</Tooltip>.
+  <Tooltip text="Master Express.js-The Node.js Framework For Your Web Development" moveTop="65">Pro Express.js</Tooltip> was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress
+    after <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps" overOrClick="click" moveTop="60">Practical Node.js</Tooltip>.
     ...
     The main focus of this post is to compare the four Node.js/Io.js frameworks: <Tooltip text="HTTP API server">Hapi</Tooltip>, <Tooltip text="Release the Kraken!">Kraken</Tooltip>, <Tooltip text="Sail away">Sails.js</Tooltip> and <Tooltip text="IBM of frameworks">Loopback</Tooltip>. There are many other frameworks to consider, but I had to draw the line somewhere.
   </div>,
