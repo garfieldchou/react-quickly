4552c4242ebe557d6091bd08622c6684156a91e8
garfield <cmgarfield@gmail.com>
2018-09-03 14:29:19 +0800

    Enhance Tooltip by making it take a property that determines whether it’s on-mouse-over or on-click behavior

--- ch10/tooltip/script.jsx
+++ ch10/tooltip/script.jsx
@@ -21,7 +21,11 @@ class Tooltip extends React.Component {
     }
     return (
       <div style={{display: 'inline'}}>
-        <span style={{color: 'blue'}} onMouseEnter={this.toggle} onMouseOut={this.toggle}>
+        <span style={{color: 'blue'}}
+          onClick={this.props.overOrClick === 'click' ? this.toggle : null}
+          onMouseEnter={this.props.overOrClick === 'over' ? this.toggle : null}
+          onMouseOut={this.props.overOrClick === 'over' ? this.toggle : null}
+          >
           {this.props.children}
         </span>
         <div className="tooltip bottom" style={style} role="tooltip">
@@ -35,9 +39,11 @@ class Tooltip extends React.Component {
   }
 }
 
+Tooltip.defaultProps = {overOrClick: 'over'}
+
 ReactDOM.render(<div>
   <Tooltip text="Master Express.js-The Node.js Framework For Your Web Development">Pro Express.js</Tooltip> was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress
-    after <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps">Practical Node.js</Tooltip>.
+    after <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps" overOrClick="click">Practical Node.js</Tooltip>.
     ...
     The main focus of this post is to compare the four Node.js/Io.js frameworks: <Tooltip text="HTTP API server">Hapi</Tooltip>, <Tooltip text="Release the Kraken!">Kraken</Tooltip>, <Tooltip text="Sail away">Sails.js</Tooltip> and <Tooltip text="IBM of frameworks">Loopback</Tooltip>. There are many other frameworks to consider, but I had to draw the line somewhere.
   </div>,
