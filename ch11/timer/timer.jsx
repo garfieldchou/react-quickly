bcd78280e1d996efe31e9a3bab1a82132e516cf0
garfield <cmgarfield@gmail.com>
2018-09-05 13:40:23 +0800

    Implement a Reset button that resets the time remaining to the original value (5, 10, or 15 seconds)

--- ch11/timer/timer.jsx
+++ ch11/timer/timer.jsx
@@ -23,9 +23,13 @@ class Button extends React.Component {
         text = 'Cancel'
         handleClick =  this.props.cancel
         break
+      case 'reset':
+        text = 'Reset'
+        handleClick =  this.props.cancel
+        break
       default:
         text = `${this.props.time} seconds`
-        handleClick = ()=>{this.props.startTimer(this.props.time)}
+        handleClick = (event)=>{this.props.startTimer(this.props.time, event)}
     }
     return <button
       type="button"
@@ -39,12 +43,17 @@ class Button extends React.Component {
 class TimerWrapper extends React.Component {
   constructor(props) {
     super(props)
-    this.state =  {timeLeft: null, timer: null, pause: false}
+    this.state =  {timeLeft: null, timer: null, pause: false, originalValue: null}
     this.startTimer = this.startTimer.bind(this)
     this.pauseResume = this.pauseResume.bind(this)
     this.cancel = this.cancel.bind(this)
+    this.reset = this.reset.bind(this)
   }
-  startTimer(timeLeft) {
+  startTimer(timeLeft, event) {
+    let originalValue = this.state.originalValue
+    if (event) {
+      originalValue = event.currentTarget.innerText.match(/(^[\d]+) seconds/)[1]
+    }
     clearInterval(this.state.timer)
     let timer = setInterval(() => {
       console.log('2: Inside of setInterval')
@@ -53,7 +62,7 @@ class TimerWrapper extends React.Component {
       this.setState({timeLeft: timeLeft})
     }, 1000)
     console.log('1: After setInterval')
-    return this.setState({timeLeft: timeLeft, timer: timer, pause: false})
+    return this.setState({timeLeft: timeLeft, timer: timer, pause: false, originalValue: originalValue})
   }
   pauseResume() {
     if (this.state.timer === null) return;
@@ -66,7 +75,13 @@ class TimerWrapper extends React.Component {
   }
   cancel() {
     clearInterval(this.state.timer)
-    this.setState({timeLeft: null, timer: null, pause: false})
+    this.setState({timeLeft: null, timer: null, pause: false, originalValue: null})
+  }
+  reset() {
+    if (this.state.originalValue) {
+      clearInterval(this.state.timer)
+      this.startTimer(this.state.originalValue)
+    }
   }
   render() {
     return (
@@ -78,6 +93,7 @@ class TimerWrapper extends React.Component {
           <Button time="15" startTimer={this.startTimer}/>
           <Button func="pauseResume" pauseResume={this.pauseResume}/>
           <Button func="cancel" cancel={this.cancel}/>
+          <Button func="reset" cancel={this.reset}/>
         </div>
         <Timer timeLeft={this.state.timeLeft}/>
       <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
