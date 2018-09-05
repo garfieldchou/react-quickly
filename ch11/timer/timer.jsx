19fb2977d4acd749b3da211d53a4e745b360c561
garfield <cmgarfield@gmail.com>
2018-09-05 09:31:26 +0800

    Convert Timer to a stateless component implemented by a fat-arrow function

--- ch11/timer/timer.jsx
+++ ch11/timer/timer.jsx
@@ -1,12 +1,10 @@
-class Timer extends React.Component {
-  render() {
-    if (this.props.timeLeft == 0) {
-      document.getElementById('end-of-time').play()
-    }
-    if (this.props.timeLeft == null || this.props.timeLeft == 0)
-      return <div/>
-    return <h1>Time left: {this.props.timeLeft}</h1>
+const Timer = (props) => {
+  if (props.timeLeft == 0) {
+    document.getElementById('end-of-time').play()
   }
+  if (props.timeLeft == null || props.timeLeft == 0)
+    return <div/>
+  return <h1>Time left: {props.timeLeft}</h1>
 }
 
 class Button extends React.Component {
